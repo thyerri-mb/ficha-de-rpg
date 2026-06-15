92b07aa52da21709de9b1be6b687f646a273985d
thyerri-mb <thyerrimoraesb@gmail.com>
2026-06-15 01:35:21 -0300

    Ficha PRATICAMENTE terminada, está funcional agora. Próximo passo é organizar os nomes das variaveis, listas, dicionários, etc. e depois partir pros sistemas mais complexos.

--- ficha_ultima.py
+++ ficha_ultima.py
@@ -25,19 +25,19 @@ while True:
 def bonus_de_raca(raca):
     if raca == "Humano":
         mensagem = "Por ser um humano, você terá 1 ponto a mais de carisma"
-        bonus = "Carisma"
+        bonus = "Carisma: "
     elif raca == "Elfo":
         mensagem = "Por ser um elfo, você terá 1 ponto a mais de intelecto"
-        bonus = "Intelecto"
+        bonus = "Intelecto: "
     elif raca == "Anão":
         mensagem = "Por ser um anão, você terá 1 ponto a mais de vigor"
-        bonus = "Vigor"
+        bonus = "Vigor: "
     elif raca == "Goblin":
         mensagem = "Por ser um goblin, você terá 1 ponto a mais de agilidade"
-        bonus = "Agilidade"
+        bonus = "Agilidade: "
     elif raca == "Ogro":
         mensagem = "Por ser um ogro, você terá 1 ponto a mais de força"
-        bonus = "Força"
+        bonus = "Força: "
     return mensagem, bonus
 m = bonus_de_raca(raca)
 print(m[0])
@@ -51,14 +51,17 @@ while True:
         classe = classe.capitalize()
         if classe in ["Bárbaro", "Guerreiro", "Paladino", "Monge", "Caçador", "Ladino", "Acólito", "Místico", "Druida", "Mago", "Clérigo", "Psiônico"]:
             break
-def vida_e_mana(classe):
+def coisas_de_classe(classe):
     if classe in ["Bárbaro", "Guerreiro", "Paladino", "Monge"]:
         arquetipo = [3, 1]
+        recomendacao = "Força e Vigor"
     elif classe in ["Caçador", "Ladino", "Acólito", "Místico"]:
         arquetipo = [2, 2]
+        recomendacao = "Agilidade e Carisma"
     elif classe in ["Druida", "Mago", "Clérigo", "Psiônico"]:
+        recomendacao = "Intelecto"
         arquetipo = [1, 3]
-    return arquetipo
+    return arquetipo, recomendacao
 
 while True:
     nivel = input("Qual o nível do seu personagem? ")
@@ -73,67 +76,70 @@ while True:
         e_numero = 0
         print("Insira um valor inteiro entre 1 e 20")     
 
-pda = 12 + nivel
-atributos = {
-    "Força": forca,
-    "Agilidade": agilidade,
-    "Vigor": vigor,
-    "Intelecto": intelecto,
-    "Carisma": carisma }
-print(atributos)
-vma = int(4+(nivel/2)) #Valor máximo de atributo.
-total = 0
-erro = 0
-while total != pda or erro == 1:
-    print(f"Distribua {pda} pontos entre os atributos:")
-    print("Força, Constituição, Destreza, Inteligência, Sabedoria e Carisma")
-    print("")
-    print(f"Por ser um {clas}, priorize colocar mais pontos em {recomendacao}")
-    print(f"Cada atributo deve ter pelo menos 1 ponto e nenhum deles deve exceder mais que {vma}")
-    print("")
-    
-    forca = int(input("Força: "))
-    vigor = int(input("Constituição: "))
-    agilidade = int(input("Destreza: "))
-    intelecto = int(input("Intelecto: "))
-    carisma = int(input("Carisma: "))
-    print("")
-    total = Fo + Co + De + In + Sa + Ca  
-#Checar se algum atributo excede o máximo permitido pelo nível.
-    if Fo > vma or Co > vma or De > vma or In > vma or Sa > vma or Ca > vma:
-        erro = 1
-        print("Há um ou mais atributos com mais pontos do que o máximo!")
-#Checar se algum atributo é zero.
-    elif Fo < 1 or Co < 1 or De < 1 or In < 1 or Sa < 1 or Ca < 1:
-        erro = 1
-        print("Há um ou mais atributos com menos pontos do que o mínino!")
-    else:
-        if total > pda:
-            erro = 1
-            print(f"Você colocou {total-pda} pontos a mais do que tem para distribuir!")
-        elif total < pda:
-            erro = 1
-            print(f"Você deixou {pda-total} pontos sobrando para distribuir!")
-        else:
-            erro = 0
-            break
-aumentador = m[1]
-atributos[aumentador] += 1
+pontos_de_atributo = 12 + nivel
 
-identificacao = vida_e_mana(classe)
-pv = (10 + nivel * identificacao[1])
-pm = (2 + nivel * identificacao[1])
+atributos = ["Força: ","Agilidade: ","Vigor: ","Intelecto: ","Carisma: "]
+
+valor_maximo = int(4+(nivel/2)) #Valor máximo de atributo.
+identificacao = coisas_de_classe(classe)
+
+print(f"Distribua {pontos_de_atributo} pontos entre os atributos:")
+print("Força, Vigor, Agilidade, Intelecto e Carisma")
+print("")
+print(f"Por ser um {classe}, priorize colocar mais pontos em {identificacao[1]}")
+print(f"Cada atributo deve ter pelo menos 1 ponto e nenhum deles deve exceder mais que {valor_maximo}")
+print("")
+
+atributinhos = 0
+valores = []
+valor_acola = pontos_de_atributo
+while atributinhos < 5:
+    if atributinhos == 0:
+        i = input("Força: ")
+    elif atributinhos == 1:
+        i = input("Vigor: ")
+    elif atributinhos == 2:
+        i = input("Agilidade: ")
+    elif atributinhos == 3:
+        i = input("Intelecto: ")
+    elif atributinhos == 4:
+        i = input("Carisma: ")
+    try:
+        e_numero = i
+        e_numero = int(e_numero)
+        while 0 < e_numero <= valor_maximo:
+            valor_acola = valor_acola-e_numero
+            if valor_acola >= 0:
+                valores.append(e_numero)
+                atributinhos += 1
+                break
+            else:
+                print("Você já excedeu seus pontos! Tente novamente")
+                atributinhos = 0
+                valor_acola = pontos_de_atributo
+                break
+    except ValueError:
+        e_numero = 1
+        atributinhos = atributinhos
+atributos1 = dict(zip(atributos, valores))
+print(atributos1)
+
+aumentador = m[1]
+atributos1[aumentador] += 1
+boost = identificacao[0]
+utopico = boost[0]
+pv = (10 + nivel * utopico)
+pm = (2 + nivel * utopico)
 
 def Ficha():
     print("")
     print("== Ficha de Personagem ==")
     print("")
-    print(f"Nome: {nome}")
-    print(f"Raça: {raca}", "	", f"Classe: {clas}")
+    print(f"Nome: {nome}", "	", f"Nível: {nivel}")
+    print(f"Raça: {raca}", "	", f"Classe: {classe}")
     print("")
-    print(f"Força: {Fo}", "	", f"Constituição: {Co}")
-    print(f"Destreza: {De}", "	", f"Inteligência: {In}")
-    print(f"Sabedoria: {Sa}", "	", f"Carisma: {Ca}")
+    for chave, valor in atributos1.items():
+        print(f"{chave}: {valor}")
     print("")
     print(f"Pontos de Vida: {pv}", "	", f"Pontos de Mana: {pm}")
     return ""
